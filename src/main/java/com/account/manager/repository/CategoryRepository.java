4369304a10f71d74c3727391d9778a5b53f60f3a
Blasko-Peter <blasko.peter.hu@gmail.com>
2020-01-23 11:54:50 +0100

    Create CategoryRepository

---
+++ src/main/java/com/account/manager/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.account.manager.repository;
+
+import com.account.manager.model.Category;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    
+
+}
