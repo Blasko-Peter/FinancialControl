2e574c0c7c98fea44cb3309499c3ac98cb4f470b
Blasko-Peter <blasko.peter.hu@gmail.com>
2020-01-23 11:53:18 +0100

    Create AccountRepository

---
+++ src/main/java/com/account/manager/repository/AccountRepository.java
@@ -0,0 +1,10 @@
+package com.account.manager.repository;
+
+import com.account.manager.model.Account;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AccountRepository extends CrudRepository<Account, Long> {
+
+    
+
+}
