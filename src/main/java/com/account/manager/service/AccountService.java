97a7cfc66c63a6aa596d2de3978825dde2cf9977
Blasko-Peter <blasko.peter.hu@gmail.com>
2020-01-23 12:54:27 +0100

    add getAccountById function to AccountService - AccountRepository

--- src/main/java/com/account/manager/service/AccountService.java
+++ src/main/java/com/account/manager/service/AccountService.java
@@ -1,10 +1,18 @@
 package com.account.manager.service;
 
+import com.account.manager.model.Account;
+import com.account.manager.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountService {
 
+    @Autowired
+    private AccountRepository accountRepository;
 
+    public Account getAccountById(long id){
+        return accountRepository.findById(id);
+    }
 
 }
