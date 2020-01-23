9eda2c465c11557ffb402d4f5cab31debb02c7ed
Blasko-Peter <blasko.peter.hu@gmail.com>
2020-01-23 12:57:48 +0100

    add getCategoryById function to CategoryService - CategoryRepository

--- src/main/java/com/account/manager/service/CategoryService.java
+++ src/main/java/com/account/manager/service/CategoryService.java
@@ -1,10 +1,18 @@
 package com.account.manager.service;
 
+import com.account.manager.model.Category;
+import com.account.manager.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryService {
 
+    @Autowired
+    private CategoryRepository categoryRepository;
 
+    public Category getCategoryById(long id){
+        return categoryRepository.findById(id);
+    }
 
 }
