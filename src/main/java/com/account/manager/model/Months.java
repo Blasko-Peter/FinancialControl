5e576e8c7f29fdb1594078154c6b3a670e94ef07
Blasko-Peter <blasko.peter.hu@gmail.com>
2020-01-23 11:12:41 +0100

    Create Months enum with name of months

---
+++ src/main/java/com/account/manager/model/Months.java
@@ -0,0 +1,37 @@
+package com.account.manager.model;
+
+public enum Months {
+
+    january(1),
+    february(2),
+    march(3),
+    april(4),
+    may(5),
+    june(6),
+    july(7),
+    august(8),
+    september(9),
+    october(10),
+    november(11),
+    december(12);
+
+    private Integer id;
+
+    Months(Integer value) {
+        this.id = value;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public static Months fromId(Integer id) {
+        for (Months month : Months.values()) {
+            if (month.getId().equals(id)) {
+                return month;
+            }
+        }
+        return null;
+    }
+
+}
