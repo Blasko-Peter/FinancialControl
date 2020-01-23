349e1cc7e7694d0c8f58b5e813f3f0dbe92e1080
Blasko-Peter <blasko.peter.hu@gmail.com>
2020-01-23 13:01:32 +0100

    add saveNewItem function to ItemService

--- src/main/java/com/account/manager/service/ItemService.java
+++ src/main/java/com/account/manager/service/ItemService.java
@@ -1,10 +1,35 @@
 package com.account.manager.service;
 
+import com.account.manager.model.Item;
+import com.account.manager.model.mapping.ItemMapping;
+import com.account.manager.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ItemService {
+    
+    @Autowired
+    private ItemRepository itemRepository;
 
+    @Autowired
+    private AccountService accountService;
 
+    @Autowired
+    private CategoryService categoryService;
+
+    public Item saveNewItem(ItemMapping itemMapping){
+        Item newItem = new Item();
+        newItem.setActualDate(itemMapping.getActualDate());
+        newItem.setAccount(accountService.getAccountById(itemMapping.getAccountId()));
+        newItem.setPlace(itemMapping.getPlace());
+        newItem.setCity(itemMapping.getCity());
+        newItem.setCategory(categoryService.getCategoryById(itemMapping.getCategoryId()));
+        newItem.setCharging(itemMapping.getCharging());
+        newItem.setCrediting(itemMapping.getCrediting());
+        newItem.setComment(itemMapping.getComment());
+        itemRepository.save(newItem);
+        return newItem;
+    }
 
 }
