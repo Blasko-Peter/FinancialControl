87e170b22b187f469f3478492047e970cc4d8275
Blasko-Peter <blasko.peter.hu@gmail.com>
2020-01-28 12:00:11 +0100

    add addNewAccount methods to AccountService

--- src/main/java/com/account/manager/service/AccountService.java
+++ src/main/java/com/account/manager/service/AccountService.java
@@ -2,9 +2,14 @@ package com.account.manager.service;
 
 import com.account.manager.model.Account;
 import com.account.manager.model.Item;
+import com.account.manager.model.mapping.AccountMapping;
 import com.account.manager.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class AccountService {
@@ -21,6 +26,16 @@ public class AccountService {
         addItemToAccountItems(account, item);
         addValueOfItemToAccountBalance(account, item);
         accountSave(account);
+    }
+
+    public void addNewAccount(AccountMapping accountMapping){
+        List<Item> items = new ArrayList<>();
+        BigDecimal newBigdecimal = new BigDecimal(0.0);
+        Account newAccount = new Account();
+        newAccount.setName(accountMapping.getName());
+        newAccount.setActive(true);
+        newAccount.setItems(items);
+        newAccount.setActualBalance(newBigdecimal);
     }
 
     private void addItemToAccountItems(Account account, Item item){
