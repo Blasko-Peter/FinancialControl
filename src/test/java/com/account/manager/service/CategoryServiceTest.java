b8f417dc9a733343f3b70a1420bb543d767ff93e
Blasko-Peter <blasko.peter.hu@gmail.com>
2020-01-29 16:54:08 +0100

    add addNewCategoryTest to CategoryServiceTest

--- src/test/java/com/account/manager/service/CategoryServiceTest.java
+++ src/test/java/com/account/manager/service/CategoryServiceTest.java
@@ -1,6 +1,8 @@
 package com.account.manager.service;
 
 import com.account.manager.model.Category;
+import com.account.manager.model.mapping.CategoryMapping;
+import com.account.manager.repository.CategoryRepository;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,10 +17,23 @@ public class CategoryServiceTest {
     @Autowired
     private CategoryService categoryService;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Test
     public void getCategoryByIdTest(){
         Category testCategory = categoryService.getCategoryById(1);
         Assert.assertNotNull(testCategory);
+    }
+
+    @Test
+    public void addNewCategoryTest(){
+        CategoryMapping newCategoryMapping = new CategoryMapping();
+        newCategoryMapping.setName("Test Category 2");
+        newCategoryMapping.setType("Charging");
+        newCategoryMapping.setIsAvtive(true);
+        categoryService.addNewCategory(newCategoryMapping);
+        Assert.assertEquals("Test Category 2", categoryRepository.findById(2).getName());
     }
 
 }
