200fd1fed8c358bb6224757a16e98b856193b22e
Blasko-Peter <blasko.peter.hu@gmail.com>
2020-01-30 12:40:13 +0100

    add getActualMonthlyItemsTest_1 and getActualMonthlyItemsTest_2 to ItemServiceTest

--- src/test/java/com/account/manager/service/ItemServiceTest.java
+++ src/test/java/com/account/manager/service/ItemServiceTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -50,6 +51,31 @@ public class ItemServiceTest {
         itemService.importItemsfromFile();
         List<Item> allItems = itemService.itemFindAllByAccountId(3);
         Assert.assertTrue(allItems.size() > 500);
+    }
+
+    @Test
+    public void getActualMonthlyItemsTest_1(){
+        int actualYear = 2024;
+        int actualMonth = 2;
+        List<Item> allItemsInActualMonth = itemService.getActualMonthlyItems(actualYear, actualMonth);
+        Assert.assertTrue(allItemsInActualMonth.size() == 0);
+    }
+
+    @Test
+    public void getActualMonthlyItemsTest_2(){
+        int actualYear = 2024;
+        int actualMonth = 2;
+        String date = "2024-02-29";
+        LocalDate localDate = LocalDate.parse(date);
+        ItemMapping itemMapping = new ItemMapping();
+        itemMapping.setActualDate(localDate);
+        itemMapping.setCategoryId(1);
+        itemMapping.setAccountId(1);
+        itemMapping.setCharging(new BigDecimal(0));
+        itemMapping.setCrediting(new BigDecimal(15));
+        Item testItem = itemService.createNewItem(itemMapping);
+        List<Item> allItemsInActualMonth = itemService.getActualMonthlyItems(actualYear, actualMonth);
+        Assert.assertTrue(allItemsInActualMonth.size() == 1);
     }
 
 }
