63ea6ed4ea3f955ba9cb9c4b2924ba2c905910b0
Blasko-Peter <blasko.peter.hu@gmail.com>
2020-01-31 14:41:50 +0100

    add @PostMapping(value = "/add-new-item-data") to ViewController - can add new item to database

--- src/main/java/com/account/manager/controller/DataController.java
+++ src/main/java/com/account/manager/controller/DataController.java
@@ -1,9 +1,12 @@
 package com.account.manager.controller;
 
+import com.account.manager.model.Item;
 import com.account.manager.model.mapping.AccountMapping;
 import com.account.manager.model.mapping.CategoryMapping;
+import com.account.manager.model.mapping.ItemMapping;
 import com.account.manager.service.AccountService;
 import com.account.manager.service.CategoryService;
+import com.account.manager.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +21,9 @@ public class DataController {
     @Autowired
     private CategoryService categoryService;
 
+    @Autowired
+    private ItemService itemService;
+
     @PostMapping(value = "/add-new-account-data")
     public String addNewAccountData(@RequestBody AccountMapping accountMapping){
         accountService.addNewAccount(accountMapping);
@@ -27,6 +33,12 @@ public class DataController {
     @PostMapping(value = "/add-new-category-data")
     public String addNewCategoryData(@RequestBody CategoryMapping categoryMapping){
         categoryService.addNewCategory(categoryMapping);
+        return "Success";
+    }
+
+    @PostMapping(value = "/add-new-item-data")
+    public String createNewItem(@RequestBody ItemMapping itemMapping){
+        Item newItem = itemService.createNewItem(itemMapping);
         return "Success";
     }
 
--- src/main/java/com/account/manager/service/ItemService.java
+++ src/main/java/com/account/manager/service/ItemService.java
@@ -14,7 +14,6 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
 
 import java.io.File;
 import java.io.FileInputStream;
