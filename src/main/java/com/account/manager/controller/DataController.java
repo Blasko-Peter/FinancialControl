090d98d49b53f87201b7ddd9380721bbe4663f16
Blasko-Peter <blasko.peter.hu@gmail.com>
2020-01-31 14:31:21 +0100

    add @PostMapping(value="/add-new-category-data") to DataController - can add new category to database

--- src/main/java/com/account/manager/controller/DataController.java
+++ src/main/java/com/account/manager/controller/DataController.java
@@ -1,7 +1,9 @@
 package com.account.manager.controller;
 
 import com.account.manager.model.mapping.AccountMapping;
+import com.account.manager.model.mapping.CategoryMapping;
 import com.account.manager.service.AccountService;
+import com.account.manager.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,9 +15,18 @@ public class DataController {
     @Autowired
     private AccountService accountService;
 
+    @Autowired
+    private CategoryService categoryService;
+
     @PostMapping(value = "/add-new-account-data")
     public String addNewAccountData(@RequestBody AccountMapping accountMapping){
         accountService.addNewAccount(accountMapping);
+        return "Success";
+    }
+
+    @PostMapping(value = "/add-new-category-data")
+    public String addNewCategoryData(@RequestBody CategoryMapping categoryMapping){
+        categoryService.addNewCategory(categoryMapping);
         return "Success";
     }
 
