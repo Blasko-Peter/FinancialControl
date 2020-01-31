7937b7bf8459ace2e5f02fdcd95b2836ea719286
Blasko-Peter <blasko.peter.hu@gmail.com>
2020-01-31 14:57:55 +0100

    add sendNewItem() function to index.js - can add new item to database from index site

--- src/main/resources/static/js/modules/index.js
+++ src/main/resources/static/js/modules/index.js
@@ -38,6 +38,46 @@ function sendNewItem1(){
     }
 }
 
+function sendNewItem(){
+    var actualDate = document.getElementById("actualDate").value;
+    var accountId = document.getElementById("accountId").selectedIndex;
+    var place = document.getElementById("place").value;
+    var city = document.getElementById("city").value;
+    var categoryId = document.getElementById("categoryId").selectedIndex;
+    var type = document.getElementById("type").value;
+    var value = document.getElementById("value").value;
+    var comment = document.getElementById("comment").value;
+    var charging = 0.0;
+    var crediting = 0.0;
+    if(actualDate == "" || accountId == 0 || place == "" || city == "" || categoryId == 0 || value == 0){
+        showDangerModal();
+    } else {
+        if(type == "Open this select menu"){
+            showDangerModal();
+        } else {
+             if(type == "Crediting"){
+                 crediting = value;
+             } else {
+                 charging = value;
+             }
+             let goodCategoryId = categoryId + 4;
+             let sendingData = {actualDate: actualDate, accountId: accountId, place: place, city: city, categoryId: goodCategoryId, charging: charging, crediting: crediting, comment: comment};
+             fetch( "/add-new-item-data", {
+               body: JSON.stringify(sendingData),
+               method:"POST",
+               headers: {
+                 "Content-Type": "application/json",
+               }
+             }).then(response => response.text())
+               .then((body) => {
+                  let answer = body;
+                  console.log(answer);
+                  location.reload();
+               });
+        }
+    }
+}
+
 function showDangerModal(){
     $('#dangerModal').modal('show');
 }
